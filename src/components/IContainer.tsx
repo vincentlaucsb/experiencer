709767bddcaf2c673d80f6d78d49d8b6956c3c85
Vincent La <vincela9@gmail.com>
2019-11-17 22:15:09 -0800

    Delete IContainer.tsx

--- src/components/IContainer.tsx
+++
@@ -1,6 +0,0 @@
-﻿export default interface IContainer<P = {}, S = {}> extends React.Component<P, S> {
-    defaultChild: JSX.Element;
-
-    addChild: (data: object) => void;
-    deleteChild: (idx: number) => void;
-}

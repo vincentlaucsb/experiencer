78a6abfe861e86a2922bb07f0672d6d3d4ba7cbd
Vincent La <vincela9@gmail.com>
2019-11-29 02:49:49 -0800

    Update Entry.tsx

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,14 +1,14 @@
 ﻿import * as React from "react";
 import ResumeComponent, { ResumeComponentProps, SelectedComponentProps, Action } from "./ResumeComponent";
 import EditButton, { AddButton, DownButton, UpButton, DeleteButton } from "./Buttons";
-import { Nonprintable } from "./Nonprintable";
 import { ButtonGroup, Button, Dropdown, DropdownButton } from "react-bootstrap";
 import AddIcon from "../icons/add-24px.svg";
-import { DropdownDivider } from "react-bootstrap/Dropdown";
 
 export interface EntryProps extends ResumeComponentProps {
-    title?: string[];
-    subtitle?: string[];
+    title?: string;
+    titleExtras?: string[];
+    subtitle?: string;
+    subtitleExtras?: string[];
 }
 
 interface EntryState {
@@ -26,16 +26,63 @@ export default class Entry extends ResumeComponent<EntryProps, EntryState> {
         };
 
         this.addTitleField = this.addTitleField.bind(this);
+        this.addSubtitleField = this.addSubtitleField.bind(this);
         this.setSelected = this.setSelected.bind(this);
     }
 
     addTitleField() {
-        let replTitle = this.props.title || [];
+        let replTitle = this.props.titleExtras || [];
         replTitle.push("");
+        this.updateData('titleExtras', replTitle);
+    }
 
-        console.log(replTitle);
+    addSubtitleField() {
+        let replTitle = this.props.subtitleExtras || [];
+        replTitle.push("");
+        this.updateData('subtitleExtras', replTitle);
+    }
 
-        this.updateData('title', replTitle);
+    getEditingControls() {
+        if (this.state.isSelected && !this.props.isPrinting) {
+            return <ButtonGroup size="sm">
+                <DropdownButton as={ButtonGroup} title="Add" id="add-options" size="sm">
+                    <Dropdown.Item onClick={this.addList}>Bulleted List</Dropdown.Item>
+                    <Dropdown.Item onClick={this.addParagraph}>Paragraph</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item onClick={this.addTitleField}>Add another title field</Dropdown.Item>
+                    <Dropdown.Item onClick={this.addSubtitleField}>Add another subtitle field</Dropdown.Item>
+                </DropdownButton>
+                <EditButton {...this.props} extended={true} />
+                <DeleteButton {...this.props} extended={true} />
+                <UpButton {...this.props} extended={true} />
+                <DownButton {...this.props} extended={true} />
+            </ButtonGroup>
+        }
+
+        return <></>
+    }
+
+    getExtras(key: string, updater: (idx: number, event: any) => void) {
+        const extraData = this.props[key];
+        if (extraData) {
+            if (this.props.isEditing) {
+                return extraData.map((text, index) =>
+                    <input key={index} onChange={updater.bind(this, index)} value={text || ""} />
+                );
+            }
+
+            return extraData.map((text, index) => <span key={index}>{text || "Enter a value"}</span>);
+        }
+
+        return <></>
+    }
+
+    getTitleExtras() {
+        return this.getExtras('titleExtras', this.updateTitleExtras);
+    }
+
+    getSubtitleExtras() {
+        return this.getExtras('subtitleExtras', this.updateTitleExtras);
     }
 
     setSelected() {
@@ -56,70 +103,48 @@ export default class Entry extends ResumeComponent<EntryProps, EntryState> {
         });
     }
 
-    updateTitle(idx: number, event: any) {
-        let replTitle = this.props.title || [];
+    updateExtras(key: string, idx: number, event: any) {
+        let replTitle = this.props[key] || [];
 
         // Replace contents
         replTitle[idx] = event.target.value;
+        this.updateData(key, replTitle);
+    }
 
-        console.log(replTitle, event);
-        this.updateData('title', replTitle);
+    updateTitleExtras(idx: number, event: any) {
+        this.updateExtras('titleExtras', idx, event);
+    }
+
+    updateSubtitleExtras(idx: number, event: any) {
+        this.updateExtras('subtitleExtras', idx, event);
     }
     
     render() {
-        let buttons = <></>
         let className = 'resume-entry';
-        const titleData = this.props.title as string[];
+        const titleExtraData = this.props.titleExtras as string[];
+        const subtitleExtraData = this.props.subtitleExtras as string[];
 
+        let title: any = this.props.title || "Enter a title";
         let subtitle: any = this.props.subtitle || "Enter a subtitle";
-        let title: any = "Enter a title";
 
         if (this.props.isEditing) {
-            if (titleData) {
-                title = titleData.map((text, index) =>
-                    <input key={index} onChange={this.updateTitle.bind(this, index)} value={text || ""} />
-                );
-            }
-            else {
-                title = <input onChange={this.updateTitle.bind(this, 0)} value="" />;
-            }
-
+            title = <input onChange={this.updateDataEvent.bind(this, "title")} value={this.props.title || ""} />;
             subtitle = <input onChange={this.updateDataEvent.bind(this, "subtitle")} value={this.props.subtitle || ""} />
-        } else if (titleData) {
-            title = titleData.map((text, index) => <span key={index}>{text}</span>);
         }
 
-        if (!this.props.isPrinting) {
-            if ((this.state.isHovering || this.state.isSelected)) {
-                className += ' resume-selected';
-            }
-
-            if (this.state.isSelected) {
-                buttons = <ButtonGroup size="sm">
-                    <DropdownButton as={ButtonGroup} title="Add" id="add-options" size="sm">
-                        <Dropdown.Item onClick={this.addList}>Bulleted List</Dropdown.Item>
-                        <Dropdown.Item onClick={this.addParagraph}>Paragraph</Dropdown.Item>
-                        <Dropdown.Divider />
-                        <Dropdown.Item onClick={this.addTitleField}>Add another title field</Dropdown.Item>
-                        <Dropdown.Item>Add another subtitle field</Dropdown.Item>
-                    </DropdownButton>
-                    <EditButton {...this.props} extended={true} />
-                    <DeleteButton {...this.props} extended={true} />
-                    <UpButton {...this.props} extended={true} />
-                    <DownButton {...this.props} extended={true} />
-                </ButtonGroup>
-            }
+        if (!this.props.isPrinting && (this.state.isHovering || this.state.isSelected)) {
+            className += ' resume-selected';
         }
 
         return <div className={className}>
-            {buttons}
+            {this.getEditingControls()}
             <h3 className="flex-row" onClick={this.setSelected}
                 onMouseEnter={() => this.setState({ isHovering: true })}
                 onMouseLeave={() => this.setState({ isHovering: false })}
             >
-                {title}
+                {title} {this.getTitleExtras()}
             </h3>
-            <p className="flex-row subtitle">{subtitle}</p>
+            <p className="flex-row subtitle">{subtitle} {this.getSubtitleExtras()}</p>
 
             {this.renderChildren()}
         </div>

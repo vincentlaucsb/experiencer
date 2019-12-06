609e3f61fa37fbde8ee7fe25bbe1a18682ed4307
Vincent La <vincela9@gmail.com>
2019-12-05 17:11:49 -0800

    Added ability to change orientation of Header items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 ﻿import * as React from "react";
 import EditButton, { DeleteButton, UpButton, DownButton } from "./Buttons";
 import ResumeComponent, { ResumeComponentProps } from "./ResumeComponent";
-import { ButtonGroup } from "react-bootstrap";
+import { ButtonGroup, DropdownButton, Dropdown } from "react-bootstrap";
 
-export default class Header extends ResumeComponent {
+export interface HeaderProps extends ResumeComponentProps {
+    orientation?: 'row' | 'column';
+}
+
+export default class Header extends ResumeComponent<HeaderProps> {
     constructor(props: ResumeComponentProps) {
         super(props);
 
@@ -11,21 +15,38 @@ export default class Header extends ResumeComponent {
             isHovering: false,
             isSelected: false
         }
+
+        this.orientColumn = this.orientColumn.bind(this);
+        this.orientRow = this.orientRow.bind(this);
     }
 
     get className(): string {
         let classNames = [super.className];
 
-        if (this.state.isSelected) {
+        if (this.props.orientation == 'row') {
+            classNames.push('flex-row-spread');
+        } else {
             classNames.push('flex-col');
         }
 
         return classNames.join(' ');
     }
 
+    orientColumn() {
+        this.updateData('orientation', 'column');
+    }
+
+    orientRow() {
+        this.updateData('orientation', 'row');
+    }
+
     getEditingMenu() {
         if (this.state.isSelected) {
             return <ButtonGroup size="sm">
+                <DropdownButton as={ButtonGroup} title="Distribute Items" id="distribute-options" size="sm">
+                    <Dropdown.Item onClick={this.orientColumn}>Top-to-bottom (column)</Dropdown.Item>
+                    <Dropdown.Item onClick={this.orientRow}>Left-to-right (row)</Dropdown.Item>
+                </DropdownButton>
                 <EditButton {...this.props} extended={true} />
                 <DeleteButton {...this.props} extended={true} />
                 <UpButton {...this.props} extended={true} />

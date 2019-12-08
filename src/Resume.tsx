e4f5fe14ab4588fd379fb8c1659f01ebfbfe39b9
Vincent La <vincela9@gmail.com>
2019-12-07 16:57:13 -0800

    Update Resume.tsx

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -513,17 +513,28 @@ class Resume extends React.Component<{}, PageState> {
                     <ul>
                         <li>Getting Started
                             <ul>
-                                <li>To create a resume, click the <a href="#" onClick={this.changeTemplate}>New</a> button in the upper left of your screen.</li>
-                                <li>A menu with different template options will pop up on the right. Don't get too hung up on any specific template, as you
-                                    can always change the details of your layout later.
+                                <li>Selecting a Template
+                                    <ul>
+                                        <li>To create a resume, click the <a href="#" onClick={this.changeTemplate}>New</a> button in the upper left of your screen.</li>
+                                        <li>A menu with different template options will pop up on the right. Don't get too hung up on any specific template, as you
+                                            can always change the details of your layout later.
+                                        </li>
+                                        <li>Click on <Button size="sm">Use this Template</Button> to start editing.</li>
+                                    </ul>
                                 </li>
-                                <li>Click on <Button size="sm">Use this Template</Button> to start editing.</li>
+                                <li>Editing Your Resume
+                                    <ul>
+                                        <li>You can edit different parts of the resume by clicking on them and using the Editing Toolbar that appears.</li>
+                                        <li>Every resume component is different, but they all have basic controls such as the ability to move them up and down,
+                                            and delete them.</li>
+                                    </ul>
+                                </li>
                             </ul>
                         </li>
                         <li>Printing Your Resume
                             <ul>
                                 <li>At any time while editing your resume, you can toggle back and forth between editing and <strong>Print Mode</strong> by
-                                    using the short cut <strong>Shift + P</strong>.</li>
+                                    using the shortcut <strong>Shift + P</strong>.</li>
                                 <li><strong>Print Mode</strong> displays a print-friendly version of your resume without any margins (unless you add them
                                     yourself).</li>
                             </ul>

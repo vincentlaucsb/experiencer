82ce00089e0520cb9cd6c394bc137c8b506c4d09
Vincent La <vincela9@gmail.com>
2019-12-11 16:26:15 -0800

    Update Helpers.test.ts

--- src/components/Helpers.test.ts
+++ src/components/Helpers.test.ts
@@ -1,5 +1,4 @@
-﻿import { isNullOrUndefined } from "util";
-import { assignIds } from "./Helpers";
+﻿import { assignIds } from "./Helpers";
 
 test('assignIDs Test', () => {
     const node = {
@@ -14,5 +13,16 @@ test('assignIDs Test', () => {
     assignIds(node);
 
     // Test
-    expect(!isNullOrUndefined(node['uuid']));
+    const topId = node['uuid'];
+    expect(topId).toBeDefined();
+
+    // Test that IDs are unique
+    node.children.forEach(
+        (child) => {
+            expect(child['uuid']).not.toBe(topId);
+            expect(child['uuid']).toBeDefined();
+        }
+    );
+
+    expect(node.children[0]['uuid']).not.toBe(node.children[1]['uuid']);
 });

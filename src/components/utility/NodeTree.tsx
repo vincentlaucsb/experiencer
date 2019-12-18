5c9c2d3de7126981a805716ad4c96debbcd85e55
Vincent La <vincela9@gmail.com>
2019-12-17 23:34:46 -0800

    Update NodeTree.tsx

--- src/components/utility/NodeTree.tsx
+++ src/components/utility/NodeTree.tsx
@@ -1,23 +1,36 @@
 ﻿import { IdType } from "./HoverTracker";
 import { assignIds, deleteAt, moveUp, moveDown, deepCopy } from "../Helpers";
+import { BasicNodeProps } from "../ResumeNodeBase";
 
-export default class ResumeNodeTree {
-    children = new Array<object>();
+/** The properties a node can be expected to have
+ *  in a JSON representation
+ * */
+export interface BasicResumeNode {
+    children?: Array<BasicResumeNode>;
+}
+
+export default class ResumeNodeTree implements BasicResumeNode {
+    children = new Array<BasicResumeNode>();
 
     constructor(children = new Array<object>()) {
         this.children = children;
     }
 
     traverse(id: IdType) {
-        let targetNode: object = this.children[id[0]],
-            parentNode: object = this;
+        let targetNode: BasicResumeNode = this.children[id[0]],
+            parentNode: BasicResumeNode = this;
 
         for (let i = 1; i < id.length; i++) {
             if (i + 1 == id.length) {
                 parentNode = targetNode;
             }
 
-            targetNode = targetNode['children'][id[i]];
+            if (targetNode.children) {
+                targetNode = targetNode.children[id[i]];
+            }
+            else {
+                throw "Target node has no children";
+            }
         }
 
         return [targetNode, parentNode];
@@ -48,16 +61,19 @@ export default class ResumeNodeTree {
      */
     addNestedChild(id: IdType, node: object) {
         let targetNode = this.getNodeById(id);
-        if (!('children' in targetNode)) {
-            targetNode['children'] = new Array<object>();
+        if (!targetNode.children) {
+            targetNode.children = new Array<object>();
         }
 
-        targetNode['children'].push(assignIds(deepCopy(node)));
+        targetNode.children.push(assignIds(deepCopy(node)));
     }
 
     deleteChild(id: IdType) {
         let parentNode = this.getParentOfId(id);
-        deleteAt(parentNode['children'], id[id.length - 1]);
+
+        if (parentNode.children) {
+            deleteAt(parentNode.children, id[id.length - 1]);
+        }
     }
 
     /**
@@ -68,7 +84,11 @@ export default class ResumeNodeTree {
         let parentNode = this.getParentOfId(id);
         const position = id[id.length - 1];
 
-        return position + 1 == parentNode['children'].length;
+        if (parentNode.children) {
+            return position + 1 == parentNode.children.length;
+        }
+
+        throw "Parent has no children";
     }
 
     updateChild(id: IdType, key: string, data: any) {
@@ -84,7 +104,12 @@ export default class ResumeNodeTree {
 
     moveUp(id: IdType) {
         let parentNode = this.getParentOfId(id);
-        parentNode = moveUp(parentNode['children'], id[id.length - 1]);
+        if (parentNode.children) {
+             moveUp(parentNode.children, id[id.length - 1]);
+        }
+        else {
+            throw "Parent has no children";
+        }
 
         // Return new ID of node
         return [
@@ -95,7 +120,12 @@ export default class ResumeNodeTree {
 
     moveDown(id: IdType) {
         let parentNode = this.getParentOfId(id);
-        moveDown(parentNode['children'], id[id.length - 1]);
+        if (parentNode.children) {
+            moveDown(parentNode.children, id[id.length - 1]);
+        }
+        else {
+            throw "Parent has no children";
+        }
 
         // Return new ID of node
         return [

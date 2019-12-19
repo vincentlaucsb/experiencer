5f79def3fbfc7c763689572cc85ff824e42a6c23
Vincent La <vincela9@gmail.com>
2019-12-19 13:13:18 -0800

    Update ToolbarOptions.tsx

--- src/components/controls/ToolbarOptions.tsx
+++ src/components/controls/ToolbarOptions.tsx
@@ -20,6 +20,22 @@ export default function ToolbarOptions(
         return arr;
     }
 
+    const removeTitleField = (node: BasicEntryProps) => {
+        if (node.title) {
+            return node.title.slice(0, node.title.length - 1);
+        }
+
+        return [];
+    };
+
+    const removeSubtitleField = (node: BasicEntryProps) => {
+        if (node.subtitle) {
+            return node.subtitle.slice(0, node.subtitle.length - 1);
+        }
+
+        return [];
+    };
+
     switch (type) {
         case Entry.name:
             return [
@@ -35,15 +51,15 @@ export default function ToolbarOptions(
                             text: 'Add another subtitle field',
                             action: () =>
                                 updateNode('subtitle', addSubtitleField(node))
-                        }/*
+                        },
                         {
                             text: 'Remove title field (from right)',
-                            action: this.removeTitleField,
+                            action: () => updateNode('title', removeTitleField(node)),
                         },
                         {
                             text: 'Remove subtitle field (from right)',
-                            action: this.removeSubtitleField,
-                        }*/
+                            action: () => updateNode('subtitle', removeSubtitleField(node))
+                        }
                     ]
                 }
             ];

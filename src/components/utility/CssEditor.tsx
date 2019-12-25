a21f79a4ffe633dd8f57f65a4d530b4e9de594f5
Vincent La <vincela9@gmail.com>
2019-12-24 17:36:57 -0800

    Update CssEditor.tsx

--- src/components/utility/CssEditor.tsx
+++ src/components/utility/CssEditor.tsx
@@ -23,7 +23,11 @@ export default class CssEditor extends React.Component<CssEditorProps> {
     /** A list of suggested CSS properties */
     static get cssProperties() {
         const properties = new Map<string, Array<string>>([
-            ['align-items', new Array<string>()],
+            ['align-content', ['start', 'center', 'space-between', 'space-around']],
+            ['align-items',
+                ['normal', 'stretch', 'center', 'start', 'end', 'flex-start',
+                'flex-end']
+            ],
             ['background', new Array<string>()],
             ['border', new Array<string>()],
             ['border-bottom', new Array<string>()],
@@ -34,29 +38,80 @@ export default class CssEditor extends React.Component<CssEditorProps> {
             ['color', new Array<string>()],
             ['content', new Array<string>()],
             ['display', new Array<string>()],
-            ['flex-direction', new Array<string>()],
+            ['flex-basis', []],
+            ['flex-direction',
+                ['column', 'column-reverse', 'row', 'row-reverse']
+            ],
+            ['flex-flow', []],
+            ['flex-grow', []],
+            ['flex-shrink', []],
+            ['flex-wrap', ['nowrap', 'wrap', 'wrap-reverse']],
+            ['float', []],
             ['font-family', new Array<string>()],
-            ['font-size', new Array<string>()],
-            ['font-weight', new Array<string>()],
+            ['font-size',
+                [
+                    'xx-small', 'x-small', 'small', 'medium',
+                    'large', 'x-large', 'xx-large', 'xxx-large',
+                    'smaller', 'larger'
+                ]
+            ],
+            ['font-size-adjust', ['none']],
+            ['font-stretch',
+                [
+                    'ultra-condensed', 'extra-condensed', 'condensed',
+                    'semi-condensed', 'normal', 'semi-expanded', 'expanded',
+                    'extra-expanded', 'ultra-expanded'
+                ]],
+            ['font-style', ['normal', 'italic', 'oblique']],
+            ['font-variant', []],
+            ['font-weight', ['normal', 'bold', 'lighter', 'bolder']],
             ['grid-column-gap', new Array<string>()],
             ['grid-row-gap', new Array<string>()],
             ['grid-template-columns', new Array<string>()],
             ['grid-template-row', new Array<string>()],
             ['height', new Array<string>()],
-            ['justify-content', new Array<string>()],
+            ['hyphens',
+                ['none', 'manual', 'auto']],
+            ['justify-content',
+                ['center', 'start', 'end', 'flex-start', 'flex-end', 'left', 'right',
+                'normal', 'space-between', 'space-around', 'space-evenly', 'stretch']
+            ],
+            ['letter-spacing', ['normal']],
+            ['list-style', [
+                'disc', 'circle', 'square', 'decimal',
+                'decimal-leading-zero',
+                'lower-roman', 'upper-roman', 'lower-greek',
+                'lower-alpha', 'lower-latin', 'upper-alpha', 'upper-latin',
+                'none'
+            ]],
+            ['list-style-type', ['disc']],
+            ['list-style-position', ['inside', 'outside']],
             ['margin', new Array<string>()],
             ['margin-bottom', new Array<string>()],
             ['margin-left', new Array<string>()],
             ['margin-right', new Array<string>()],
             ['margin-top', new Array<string>()],
+            ['max-height', []],
+            ['max-width', []],
+            ['min-height', []],
+            ['min-width', []],
+            ['overflow-wrap',
+                ['normal', 'anywhere', 'break-word']],
             ['padding', new Array<string>()],
             ['padding-bottom', new Array<string>()],
             ['padding-left', new Array<string>()],
             ['padding-right', new Array<string>()],
             ['padding-top', new Array<string>()],
-            ['text-align', new Array<string>()],
-            ['text-transform', new Array<string>()],
-            ['width', new Array<string>()]
+            ['position', []],
+            ['text-align',
+                ['left', 'right', 'center', 'justify']],
+            ['text-indent', ['hanging']],
+            ['text-transform',
+                ['capitalize', 'uppercase', 'lowercase', 'none', 'full-width',
+                'full-size-kana']],
+            ['width', new Array<string>()],
+            ['word-break', ['normal', 'break-all', 'keep-all']],
+            ['word-spacing', ['normal']]
         ]);
 
         // 'initial', 'inherit', 'unset' apply to all CSS properties
@@ -67,6 +122,12 @@ export default class CssEditor extends React.Component<CssEditorProps> {
                 values.push('initial', 'inherit', 'unset');
                 properties.set(k, values);
             }
+        }
+
+        /** Copy properties */
+        let alignItems = properties.get('align-items');
+        if (alignItems) {
+            properties.set('align-self', alignItems);
         }
 
         return properties;
